789fdc80f59d6ce8ecb703e690a4a793754f4e37
Matt Gibson <matt@inaura.net>
2012-04-18 13:27:29 +0100

    Lang file

---
+++ lang/en/local_sauceondemand.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * SauceOnDemand unit test plugin
+ *
+ * @package    local
+ * @subpackage sauceondemand
+ * @copyright  2012 ULCC {@link http://ulcc.ac.uk}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'SauceOnDemand Unit tests';
+$string['testusername'] = 'Test account username for this site';
+$string['testuserpass'] = 'Test account password for this site';
+$string['saucelabsusername'] = 'Sauce Labs username';
+$string['saucelabstoken'] = 'Sauce Labs API token (from your profile)';
